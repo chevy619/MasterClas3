9d37c75caec921d3087d221d29b2a029aaf8a967
juancorredor <juansc619@hotmail.com>
2022-10-09 21:11:46 -0500

    Cambio propertis y archivo machine

--- src/main/java/com/usa/misiontic/MasterClass3/entities/Machine.java
+++ src/main/java/com/usa/misiontic/MasterClass3/entities/Machine.java
@@ -22,7 +22,7 @@ public class Machine implements Serializable {
     private Integer id;
     private String name;
     private String brand;
-    private Integer year;
+    private Integer years;
     private String description;
     
     @ManyToOne
@@ -55,11 +55,11 @@ public class Machine implements Serializable {
     }
 
     public Integer getYear() {
-        return year;
+        return years;
     }
 
     public void setYear(Integer year) {
-        this.year = year;
+        this.years = year;
     }
 
     public Category getCategory() {
