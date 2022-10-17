3374f59271ad0ec1c9284bc3f18a8c7334655ae3
sebastian <juansc619@hotmail.com>
2022-10-17 11:59:26 -0500

    Cambio delete service category

--- src/main/java/com/usa/misiontic/MasterClass3/controller/CategoryController.java
+++ src/main/java/com/usa/misiontic/MasterClass3/controller/CategoryController.java
@@ -6,6 +6,7 @@ import com.usa.misiontic.MasterClass3.service.CategoryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +46,7 @@ public class CategoryController {
         return categoryService.update(category);
     }
     
-    @PutMapping("/delete")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public boolean delete(@PathVariable("id") int categoryId){
         return categoryService.delete(categoryId);
